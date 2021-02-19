09ff8257288077b503553bd60c4a67cff00837e3
Vitaliy <mv703021@gmail.com>
2021-02-19 13:44:10 +0200

    Changes done

--- HomeWork_1/app.js
+++ HomeWork_1/app.js
@@ -1,32 +1,75 @@
 const fs = require('fs');
-const primaryUsers = [
+const path = require('path');
+
+const randomUsers1 = [
     {name: 'Alex', gender: 'male'},
     {name: 'Viktor', gender: 'male'},
     {name: 'Alina', gender: 'female'},
     {name: 'Oksana', gender: 'female'},
     {name: 'Igor', gender: 'male'}
 ];
-
-for (const user of primaryUsers) {
-    fs.writeFile(`${user.name}.json`, JSON.stringify(user), err => {
+// з 11 по 23 строчку просто створюю файлики з юзерами в двох папках 18_00 і 20_00
+for (const user of randomUsers1) {
+    fs.writeFile(path.join(__dirname, '18_00', `${user.name}.json`), JSON.stringify(user), err => {
         if (err) console.log(err);
+        return;
+    })
+}
+for (const user of randomUsers1) {
+    fs.writeFile(path.join(__dirname, '20_00', `${user.name}.json`), JSON.stringify(user), err => {
+        if (err) console.log(err);
+        return;
     })
 }
 
-for (const user of primaryUsers) {
-    fs.readFile(`${user.name}.json`, (err, data) => {
-        if (err) console.log(err);
-        else {
-            const usersData = JSON.parse(data);
-            if (usersData.gender === 'male') {
-                fs.writeFile(`./20_00/${usersData.name}`, JSON.stringify(usersData), err => {
-                    if (err) console.log(err);
-                })
-            } else {
-                fs.writeFile(`./18_00/${usersData.name}`, JSON.stringify(usersData), err => {
-                    if (err) console.log(err);
+//тут витягую всіх чоловіків і перекидаю в папку 22_00
+fs.readdir(path.join(__dirname, '18_00'), (err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    files.forEach(el => {
+        fs.readFile(path.join(__dirname, '18_00', el), (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            let user = JSON.parse(res);
+            if (user.gender === "male") {
+                fs.rename(path.join(__dirname, '18_00', `${user.name}.json`), path.join(__dirname, '20_00', `${user.name}.json`), err => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
                 })
             }
-        }
+        })
     })
-}
+})
+
+//тут роблю всьо то саме тільки з папки 20_00 витягаю жінок і перекидую в 18_00
+fs.readdir(path.join(__dirname, '20_00'), (err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    files.forEach(el => {
+        fs.readFile(path.join(__dirname, '20_00', el), (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            let user = JSON.parse(res);
+            if (user.gender === "female") {
+                fs.rename(path.join(__dirname, '20_00', `${user.name}.json`), path.join(__dirname, '18_00', `${user.name}.json`), err => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                })
+            }
+        })
+    })
+})
+
+
--- Practics/task2/app.js
+++ Practics/task2/app.js
@@ -1,33 +1,54 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const path = require('path');
+//
+// const empty = [];
+
+
+// fs.readdir(__dirname, (err, files) => {
+//     if (err) console.log(err);
+//     files.shift()
+//     files.forEach(el => {
+//         console.log(el, "element*******");
+//         fs.readdir(path.join(__dirname, el,), (err, data) => {
+//             if (err) console.log(err);
+//             for (const item of data) {
+//                 fs.readFile(path.join(__dirname, el, item), (err, response) => {
+//                     if (err) console.log(err);
+//                     empty.push(JSON.parse(response));
+//                     console.log('*************************************');
+//                 })
+//             }
+//             setTimeout(() => {
+//                 console.log(empty)
+//             }, 2000)
+//         })
+//     })
+// })
+
+
+//const chalk = require('chalk') бібліотечка для кастомних шрифтів
+const {promisify} = require('util')
+const fs = require('fs')
+const fsExtra = require('fs-extra')
 const path = require('path');
-
-const empty = [];
-
-
-fs.readdir(__dirname, (err, files) => {
-    if (err) console.log(err);
-    files.shift()
-    files.forEach(el => {
-        console.log(el, "element*******");
-        fs.readdir(path.join(__dirname, el,), (err, data) => {
-            if (err) console.log(err);
-            for (const item of data) {
-                fs.readFile(path.join(__dirname, el, item), (err, response) => {
-                    if (err) console.log(err);
-                    empty.push(JSON.parse(response));
-                    console.log('*************************************');
-                })
-            }
-            setTimeout(() => {
-                console.log(empty)
-            }, 2000)
-        })
-    })
-})
+const readFile = promisify(fs.readFile);
 
 
+const func = async () => {
+    try {
+        const data = await fsExtra.readFile(__dirname)
+        console.log(data);
+
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+
+func()
 
 
+// console.log(process.cwd());
 
-
-
+// // 2 - fs-extra
+// const data = await fsExtra.readFile(path.join(process.cwd(), 'test.txt'))
+// console.log(chalk.blue.bold.underline(data.toString()))
