1643a1f3dc3a5120277092096260b4aac586ae0a
Vitaliy <mv703021@gmail.com>
2021-02-19 15:40:28 +0200

    dsadasd

--- HomeWork1_2/app.js
+++ HomeWork1_2/app.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 
-function getFiles(dir, files_) {
+function getFiles(dir, files_) { //цю функцію я скомуніздив з overstakFlow щоб получити всі шляхи до всіх папочок
+    // помню шо ти казав синхронщину не юзати але подумав, шо для такого таску піде))
     files_ = files_ || [];
     const files = fs.readdirSync(dir);
     for (let i in files) {
         let name = dir + '/' + files[i];
         if (fs.statSync(name).isDirectory()) {
             getFiles(name, files_);
+
         } else {
             files_.push(name);
         }
@@ -18,6 +20,8 @@ const result = getFiles(`${__dirname}/dir1`);
 
 console.log(result);
 
+// ну і тут я просто перебирав по всіх шляхах і сплітував останній елемент з кожного елемента масиву,
+// бо останній елемент це назва якраз файлику
 // result.forEach(el => {
 //     fs.stat(el, (err, stat) => {
 //         if (err) console.log(err);
